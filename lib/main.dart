72fe3e2400b9cbbb49b99fbdae82750c19f6478c
Hunter Peress <772917+hunterpp@users.noreply.github.com>
2022-06-18 07:37:44 -0400

    pag view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pagination_view/pagination_view.dart';
 import 'package:riverpod_child_provider/dogs.dart';
 
 void main() {
@@ -30,6 +31,34 @@ class MyHomePage extends ConsumerStatefulWidget {
   ConsumerState<MyHomePage> createState() => _MyHomePageState();
 }
 
+Widget myWidget(BuildContext context, WidgetRef ref) {
+  return PaginationView<String>(
+    paginationViewType: PaginationViewType.listView,
+    itemBuilder: (BuildContext context, String fid, int index) {
+      return Text(ref.watch(dogProvider(DogType.shiba)).setMe,
+          style: const TextStyle(fontSize: 25));
+    },
+    pageFetch: pageFetch,
+    pullToRefresh: true,
+    onError: (dynamic error) => const Center(
+      child: Text('Some error occured. Sorry!'),
+    ),
+    onEmpty: const Text('Sorry! This is empty'),
+    bottomLoader: const Center(
+      child: CircularProgressIndicator(),
+    ),
+    initialLoader: const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+}
+
+Future<List<String>> pageFetch(int offset) async {
+  final List<String> nextUsersList =
+      List.generate(5, (int index) => ' - $offset$index');
+  return nextUsersList;
+}
+
 class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -51,6 +80,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               child: Text(ref.watch(dogProvider(DogType.pittie)).setMe,
                   style: const TextStyle(fontSize: 25)),
             ),
+            Flexible(child: myWidget(context, ref)),
             TextButton(
                 onPressed: () {
                   ref.read(dogProvider(DogType.shiba).notifier).setIt("Hey");
